521073fdd764ea5fb71485af2a87b9020791efb1
Mário Garrido <skinie18@gmail.com>
2013-03-12 18:19:45 +0000

    Init project

---
+++ PhysicWavesProject/src/physicwavesproject/PhysicWavesProject.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physicwavesproject;
+
+/**
+ *
+ * @author skinie18
+ */
+public class PhysicWavesProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
